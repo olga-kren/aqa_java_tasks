e36bc7457fcc32cee305e5126bb50aef0eae0b2e
OlgaKr <olga2kg@gmail.com>
2020-10-22 19:03:16 +0300

    Task2 fixed

--- src/main/java/lesson1/Task2.java
+++ src/main/java/lesson1/Task2.java
@@ -10,8 +10,8 @@ public class Task2 {
 
     public static void main(String[] args) {
         String s = "Olga";
-        for (int i = 0; i < 10; i++){
-            for (int j = 0; j < 5; j++){
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 9; j++){
                 System.out.print(s+" ");}
             System.out.println(s);
         }
