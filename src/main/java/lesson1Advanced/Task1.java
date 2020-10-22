ea660a5f82485050024d2dff0d254d655282b5e2
OlgaKr <olga2kg@gmail.com>
2020-10-22 18:56:17 +0300

    Task1 advanced added

--- src/main/java/lesson1Advanced/Task1.java
+++ src/main/java/lesson1Advanced/Task1.java
@@ -1,15 +1,24 @@
 package lesson1Advanced;
 
+import java.util.Arrays;
+
 public class Task1 {
     /* Минимум n чисел
     Написать функцию, которая вычисляет минимум из массива чисел.
     */
     public static void main(String[] args) {
+        int[] num = new int[]{2, 4, 6, 1, 90, 2, 0};
+        int[] num1 = new int[]{2,4,6};
+
+        Arrays.sort(num);
+        Arrays.sort(num1);
         //System.out.println(min([2,4,6]));
         //System.out.println(min([2,4,6,1,90,2,0]));
+        System.out.println("Minimum = " + num[0]);
+        System.out.println("Minimum = " + num1[0]);
+
     }
 
-    public static int min(int[] a) {
-        return 0;
-    }
+
 }
+
