a24247388a0306550e16aa8c83d8e597e1dc6dc8
OlgaKr <olga2kg@gmail.com>
2020-10-26 19:13:54 +0200

    TestTask1 added

--- src/test/java/test/java/TestTask1.java
+++ src/test/java/test/java/TestTask1.java
@@ -1,3 +1,4 @@
+package test.java;
 
 import lesson1.Task1;
 import org.testng.annotations.DataProvider;
@@ -10,14 +11,15 @@ public class TestTask1 {
     @Test(dataProvider = "dp")
     public void min(int a, int b, int c, int expectedMin) {
         int actualMin = Task1.min(a, b, c);
-        assertEquals(expectedMin, actualMin, String.format("Expected  %d to be equal %d", expectedMin, actualMin));
+        assertEquals(expectedMin, actualMin, String.format("Expected %d to be min than %d", expectedMin, actualMin));
     }
     @DataProvider(name = "dp")
     public Object[][] provider() {
         return new Object[][]{
                 {3,5,2,2},
                 {2,3,5,2},
-                {1,2,3,2}
+                {1,2,3,1},
+                {-1,0,20,-1}
         } ;
     }
 }
