ec6cd25d99e780af2924e4730b42fc12e1b5fe1b
OlgaKr <olga2kg@gmail.com>
2020-10-31 18:30:31 +0200

    TaskLesson8 updated

---
+++ src/test/java/HWLesson8/HWLesson8.java
@@ -0,0 +1,69 @@
+package test.java.HWLesson8;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class HWLesson8 {
+    WebDriver driver;
+    WebDriverWait wait;
+    By signInBy = By.xpath("//*[@id='nav-link-accountList']/div");
+    By createYourAccountBy = By.id("createAccountSubmit");
+    By createYourAccount2By = By.xpath("//*[@id='continue']");
+    By nameInputBy = By.cssSelector("#ap_customer_name");
+    By emailInputBy = By.cssSelector("#ap_email");
+    By passwordBy = By.cssSelector("#ap_password");
+//    By nameAlertBy = By.id("auth-customerName-missing-alert");
+//    By emailAlertBy = By.id("auth-email-missing-alert");
+//    By passAlertBy = By.id("auth-password-missing-alert");
+
+    String expectedBorderColor = "rgb(221, 0, 0)";
+    @BeforeMethod
+    public void initializeBrowser() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new ChromeDriver(chromeOptions);
+        wait = new WebDriverWait(driver, 10, 500);
+    }
+    @Test
+    public void CreateAccountTest(){
+        driver.get("https://www.amazon.com/");
+        WebElement signInBtn = driver.findElement(signInBy);
+        signInBtn.click();
+        WebElement createYourAccountBtn = driver.findElement(createYourAccountBy);
+        createYourAccountBtn.click();
+        WebElement createYourAccount2Btn = driver.findElement(createYourAccount2By);
+        createYourAccount2Btn.click();
+        WebElement nameInput = driver.findElement(nameInputBy);
+        WebElement emailInput = driver.findElement(emailInputBy);
+        WebElement passwordInput = driver.findElement(passwordBy);
+
+        WebElement nameAlert = driver.findElement(By.id("auth-customerName-missing-alert"));
+        WebElement emailAlert = driver.findElement(By.id("auth-email-missing-alert"));
+        WebElement passAlert = driver.findElement(By.id("auth-password-missing-alert"));
+
+        String actualNameBorderClr = nameInput.getCssValue("border-color");
+        String actualEmailBorderClr = emailInput.getCssValue("border-color");
+        String actualPassBorderClr = passwordInput.getCssValue("border-color");
+
+        assertEquals(actualNameBorderClr, expectedBorderColor);
+        assertEquals(actualEmailBorderClr, expectedBorderColor);
+        assertEquals(actualPassBorderClr, expectedBorderColor);
+        assertEquals(nameAlert.getText(), "Enter your name");
+        assertEquals(emailAlert.getText(), "Enter your email");
+        assertEquals(passAlert.getText(), "Enter your password");
+
+    }
+    @AfterMethod
+       public void finalizeBrowser() {
+       driver.quit();
+    }
+}
