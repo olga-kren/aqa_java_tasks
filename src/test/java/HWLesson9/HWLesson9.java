a316b1a64b131a6dec8bd20b50392b279e3a5cc1
OlgaKr <olga2kg@gmail.com>
2020-11-05 22:12:21 +0200

    HWLesson9 updated

--- src/test/java/HWLesson9/HWLesson9.java
+++ src/test/java/HWLesson9/HWLesson9.java
@@ -1,4 +1,17 @@
 package test.java.HWLesson9;
 
-public class HWLesson9 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import test.java.TestBaseSetup;
+
+public class HWLesson9 extends TestBaseSetup {
+    By searchInputBy = By.id("twotabsearchtextbox");
+
+    @Test
+    public void searchIphoneTest(){
+        driver.get("https://www.amazon.com/");
+        WebElement searchInput = driver.findElement(searchInputBy);
+        searchInput.click();
+    }
 }
--- src/test/java/TestBaseSetup.java
+++ src/test/java/TestBaseSetup.java
@@ -7,7 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBaseSetup {
-    WebDriver driver;
+    public WebDriver driver;
 
 
     @BeforeMethod
