eebe7e004bfa03e9b97e1c83dbc96138c1994298
OlgaKr <olga2kg@gmail.com>
2020-11-20 13:43:52 +0200

    HWLesson11 updated

--- src/test/java/HWLesson11/HWLesson11.java
+++ src/test/java/HWLesson11/HWLesson11.java
@@ -1,16 +1,19 @@
-package test.java.HWLesson10;
+package test.java.HWLesson11;
 
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 import test.java.TestBaseSetup;
 import test.java.pages.HomePage;
 import test.java.pages.SearchResultPage;
 import test.java.utils.PropertyLoader;
 
 import java.util.List;
+
 import static org.testng.Assert.assertTrue;
 
-public class HWLesson10 extends TestBaseSetup {
+public class HWLesson11 extends TestBaseSetup {
     HomePage homePage;
     SearchResultPage searchResultPage;
 
@@ -20,7 +23,7 @@ public class HWLesson10 extends TestBaseSetup {
         searchResultPage = new SearchResultPage(driver);
 
     }
-    @Test(dataProvider = "LaptopBrands")
+    @Test
     public void searchLaptopPage (String brand, String value){
         homePage
                 .open()
@@ -43,21 +46,23 @@ public class HWLesson10 extends TestBaseSetup {
         }
         searchResultPage.clearSelection();
     }
+/*
     @DataProvider(name = "LaptopBrands")
     public Object[][] dataProvider(){
         return new Object[][]{
                 {"hp", "//li[@id=\"p_89/HP\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
                 {"acer", "//li[@id=\"p_89/Acer\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
-                {"asus", "//li[@id=\"p_89/ASUS\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
-                {"lenovo", "//li[@id=\"p_89/Lenovo\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
-                {"dell", "//li[@id=\"p_89/Dell\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
-                {"microsoft", "//li[@id=\"p_89/Microsoft\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
-                {"apple", "//li[@id=\"p_89/Apple\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
-                {"chuwi", "//li[@id=\"p_89/CHUWI\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
-                {"lg", "//li[@id=\"p_89/LG\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
-                {"jumper", "//li[@id=\"p_89/jumper\"]//i[@class=\"a-icon a-icon-checkbox\"]"}
+//                {"asus", "//li[@id=\"p_89/ASUS\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
+//                {"lenovo", "//li[@id=\"p_89/Lenovo\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
+//                {"dell", "//li[@id=\"p_89/Dell\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
+//                {"microsoft", "//li[@id=\"p_89/Microsoft\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
+//                {"apple", "//li[@id=\"p_89/Apple\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
+//                {"chuwi", "//li[@id=\"p_89/CHUWI\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
+//                {"lg", "//li[@id=\"p_89/LG\"]//i[@class=\"a-icon a-icon-checkbox\"]"},
+//                {"jumper", "//li[@id=\"p_89/jumper\"]//i[@class=\"a-icon a-icon-checkbox\"]"}
         };
     }
+*/
 
 }
 
